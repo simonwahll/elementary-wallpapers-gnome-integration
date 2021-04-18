2c082a1513e33133d4efc062ad20d93c8d5ad1b2
Simon Vall <simon@Neptune.localdomain>
2021-04-18 19:12:20 +0200

    Redirect mkdir error to /dev/null

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-mkdir /usr/share/gnome-background-properties
+mkdir /usr/share/gnome-background-properties 2> /dev/null
 cp usr/share/gnome-background-properties/elementary-backgrounds.xml /usr/share/gnome-background-properties/
