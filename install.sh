b9fc9e3a0be69fb2b6a2f47a4072dc4414b08281
Simon Vall <wahll@protonmail.ch>
2021-05-06 20:45:10 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 mkdir /usr/share/gnome-background-properties 2> /dev/null
-install -Dm644 usr/share/gnome-background-properties/elementary-backgrounds.xml /usr/share/gnome-background-properties/
+install -Dm644 usr/share/gnome-background-properties/elementary-backgrounds.xml -t /usr/share/gnome-background-properties/
